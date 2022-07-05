d94f7aa10dbb6f2851212c54ab94e96b6c25755b
gbenga-MA <michaeladeosun@ymail.com>
2022-07-05 20:57:59 +0100

    Task 0

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,18 +1,23 @@
-#include "main.h"
+#include "stdio.h"
+
 /**
-*main - prints putchar
-*Return: always 0
-*/
+ * main - prints _putchar
+ *
+ * Description: prints _putchar
+ *
+ * Return: 0 (Success)
+ */
+
 int main(void)
 {
-_putchar('_');
-_putchar('p');
-_putchar('u');
-_putchar('t');
-_putchar('c');
-_putchar('h');
-_putchar('a');
-_putchar('r');
-_putchar('\n');
-return (0);
+	char text[10] = "_putchar";
+	int i = 0;
+
+	for (i = 0; i < 9; i++)
+	{
+		_putchar(text[i]);
+	}
+	_putchar('\n');
+
+	return (0);
 }
