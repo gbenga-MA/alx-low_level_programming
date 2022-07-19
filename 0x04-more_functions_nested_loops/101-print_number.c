836150a18eb1c42200fda76792560bac0493dcff
gbenga-MA <michaeladeosun@ymail.com>
2022-07-19 16:56:23 +0100

    Task 1

--- 0x04-more_functions_nested_loops/101-print_number.c
+++ 0x04-more_functions_nested_loops/101-print_number.c
@@ -65,4 +65,13 @@ void print_number(int n)
 	{
 		_putchar(48);
 	}
+#include "main.h"
+
+/**
+ * _isupper - checks for uppercase
+ * @c: character to checked for case
+ *
+ * Return: 1 if uppercase, 0 if not uppercase
+ */
+
 }
