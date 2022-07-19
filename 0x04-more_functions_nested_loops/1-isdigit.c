4254701e6af2ceeef8109b37ac375cb7a39a3241
gbenga-MA <michaeladeosun@ymail.com>
2022-07-19 16:59:07 +0100

    Task 1

--- 0x04-more_functions_nested_loops/1-isdigit.c
+++ 0x04-more_functions_nested_loops/1-isdigit.c
@@ -1,15 +1,12 @@
 #include "main.h"
 
 /**
- * _isdigit - checks for digithood
- * @c: character to checked for digit membership
- *
- * Return: 1 if digit, 0 if not digit
+ * _isdigit - function to check for a digit
+ * @c: an integer argument
+ * Return: success
  */
 
 int _isdigit(int c)
 {
-	if (c >= '0' && c <= '9')
-		return (1);
-	return (0);
+	return (c >= '0' && c <= '9');
 }
